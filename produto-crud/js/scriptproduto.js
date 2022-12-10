4c1d39dd0dabbdbfe1a771d793edb8710775673a
isaquelevi <isaquelevi@gmail.com>
2022-12-10 19:59:32 -0300

    atualizações no projeto frontend

--- produto-crud/js/scriptproduto.js
+++ produto-crud/js/scriptproduto.js
@@ -7,51 +7,57 @@ function listar() {
     xhttp.send();
     xhttp.onload = function () {
         lista = this.responseText;
-        lista = JSON.parse(lista);    
-        texto = "";
-        i = 0;
-        for (const u of lista) {
-            texto += `<tr onclick='editar(${i})'><td>${u.nome}</td><td>${u.descricao}</td><td>${u.valor}</tr>`;
-            i++;
+        lista = JSON.parse(lista);
+        corpoTabela = "";
+        for (i in lista) {
+            produto = lista[i];
+            corpoTabela += `
+                    <tr onclick='editar(${i})'>
+                    <td>${produto.nome}</td>
+                    <td>${produto.descricao}</td>
+                    <td>${produto.valor}</td>
+                    </tr>`;
         }
-        document.getElementById('lista').innerHTML = texto;
+        document.querySelector("#corpoTabela").innerHTML = corpoTabela;
     }
 }
 
+listar();
+
 function editar(i) {
-    u = lista[i];
-    document.getElementById("nome").value = u.nome;
-    document.getElementById("descricao").value = u.descricao;
-    document.getElementById("valor").value = u.valor;
-    document.getElementById("id").value = u.id;
+    produto = lista[i];
+    document.querySelector("#nome").value = produto.nome;
+    document.querySelector("#descricao").value = produto.descricao;
+    document.querySelector("#valor").value = produto.valor;
+    document.querySelector("#id").value = produto.id;
 }
 
 function gravar() {
-    var item = {};
-    item.nome = document.getElementById("nome").value;
-    item.descricao = document.getElementById("descricao").value;
-    item.valor = document.getElementById("valor").value;
-    item.id = document.getElementById("id").value;
-    if (item.id > 0) {
-        acao = "PUT"; 
-    } else {
-        acao = "POST";
-    }
+    produto = {};
+    produto.nome = document.getElementById("nome").value;
+    produto.descricao = document.getElementById("descricao").value;
+    produto.valor = document.getElementById("valor").value;
+
+    produto.id = document.getElementById("id").value;
+
+    acao = (produto.id > 0) ? "PUT" : "POST";
 
     xhttp.open(acao, api);
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(JSON.stringify(item));
+    xhttp.send(JSON.stringify(produto));
     xhttp.onload = function () {
+        // this.responseText;
         listar();
         limpar();
     }
+
 }
 
 function limpar() {
-    document.getElementById("nome").value = "";
-    document.getElementById("descricao").value = "";
-    document.getElementById("valor").value = "";
-    document.getElementById("id").value = "";
+    document.querySelector("#nome").value = "";
+    document.querySelector("#descricao").value = "";
+    document.querySelector("#valor").value = "";
+    document.querySelector("#id").value = "";
 }
 
 function apagar() {
@@ -59,9 +65,9 @@ function apagar() {
     xhttp.open("DELETE", api + id);
     xhttp.send();
     xhttp.onload = function () {
-        alert(this.responseText);
+        msg = this.responseText;
+        alert(msg);
         listar();
         limpar();
     }
 }
-listar();
