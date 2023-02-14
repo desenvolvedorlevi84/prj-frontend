75a9053ae02745a57b624923ddd295205b3e8bcb
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-02-13 22:37:32 -0300

    inicio da criacao da tabela dos produtos do carrinho

--- fake-api/produto.js
+++ fake-api/produto.js
@@ -3,6 +3,6 @@
     {"nome":"Baixo Fodera","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/contra_baixo_fodera.png","valor":8.200},
     {"nome":"Guitarra Fender","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/Guitarra_Fender_Telecaster_Affinity.png","valor":6.800},
     {"nome":"Mesa Digital Midas","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/mesa-de-som-midas32.png","valor":65.000},
-    {"nome":"Teclado Yamaha XF6","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/teclado_sintetizador_yamaha_motif_xf6.png","valor":9.250},
-    {"nome":"Violao Taylor","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/violao_taylor.png","valor":11.250}
+    {"nome":"Teclado Yamaha XF6","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/teclado_sintetizador_yamaha_motif_xf6.png","valor":9.200},
+    {"nome":"Violao Taylor","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/violao_taylor.png","valor":11.200}
 ]
